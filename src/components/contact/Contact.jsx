8fd86c77cf5647d4db978845e85ca7ffbe6ea22c
Sunnychandr <sunny.chandra310505@gmail.com>
2022-08-26 10:08:34 +0700

    my changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,11 +9,11 @@ const Contact = () => {
   return (
     <section id="contact">
       <h1 classsName='title__name'> <b> Get In Touch </b> </h1>
-      <p className='title__desc'>Although I’m not currently looking for any new opportunities, my inbox is always open. <br></br>Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+      <p className='title__desc'>Now I’m looking for any new opportunities, my inbox is always open. <br></br>Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
       
       
         <div className="my__icon-carrier">
-          <a href="https://github.com/Sunnychandrz" target="_blank" data-aos="fade-up" ><img src={GITHUB} /></a>
+          <a href="https://github.com/Sunnychandr" target="_blank" data-aos="fade-up" ><img src={GITHUB} /></a>
           <a href="https://www.linkedin.com/in/sunny-chandra-b89021207/" target="_blank" data-aos="fade-up" ><img src={LINKEDIN} /></a>
           <a href="mailto:sunny.chandra310505@gmail.com" target="_blank" data-aos="fade-up" > <img src={EMAIL} /></a>
           <a href="https://instagram.com/sunchand.js" target="_blank" data-aos="fade-up" ><img src={INSTA} /></a>
